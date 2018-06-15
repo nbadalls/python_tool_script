1cd925a7b76d6c4c9d956c9b602d8737d322b5a3
nbadalls <1277046020@qq.com>
2018-06-15 15:45:19 +0800

    version1

--- run_script.py
+++ run_script.py
@@ -6,6 +6,7 @@ Created on Fri Jun 15 10:41:45 2018
 """
 
 import text_process
+import create_list
 
 def run_script():
     input_path = "/home/zkx/Data_sdb/TrainData_landmarks/single_mtcnn_landmark/Life_beidi-pad-cap_130_landmarks.txt"
@@ -13,8 +14,16 @@ def run_script():
     select_landmark = text_process.TextProcess(input_path, output_path)
     select_landmark.selected_certain_part_from_landmark_file('.JPG')
     
+def createlist():
     
-    
+      input_path = "/home/zkx/Data_sdb/TrainData/shot1600"
+      dst_path = "/home/zkx/Data_sdb/TrainData/Data_sdd/Patches_mtcnn/MultiPatches_list/single_folder_list"
+      get_list = create_list.CreateList(input_path, dst_path)
+      
+      num = 10
+      add_prefix = None
+      get_list.create_condition_image_list(num, add_prefix)
+      
 if __name__ == '__main__':
     
-    run_script()
+    createlist()
--- utility.py
+++ utility.py
@@ -5,6 +5,8 @@ Created on Fri Jun 15 10:31:04 2018
 @author: zkx-97
 """
 import sys
+import os
+
 
 def read_file(file_path):
     f = open(file_path, 'r')
@@ -20,3 +22,7 @@ def show_process_percentage(index, sum_num, display_gap):
     if index == sum_num:
         sys.stdout.write('{}/{}\t100.0%\n'.format(index, sum_num))
         sys.stdout.flush()
+        
+def create_dir(folder_path):
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
